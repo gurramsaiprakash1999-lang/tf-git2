773810ddb4c6e3deaea04e63cedcf18d3767ffb0
Prakash <gurramsaiprakash1999@gmail.com>
2026-07-29 10:24:18 +0000

    modified perfectly

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3-tf-07-2026-bucket-2" # Create this bucket manually in AWS first!
+    bucket = "s3-tf-07-2026-bucket-3-new" # Create this bucket manually in AWS first!
     key    = "terraform.tfstate"
     region = "ap-south-2"
   }
